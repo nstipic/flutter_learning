410ec7a808e1b83e0e78d01c34876d716b8d6527
nstipic <alokin544@gmail.com>
2022-11-09 23:17:28 +0100

    Pushing project to Git

---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learning page'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/test.jpg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(color: Colors.black),
+        ],
+      ),
+    );
+  }
+}
